eca40f3db42faa37b50aae92ee0dc0e91092bec7
avagagner <46656654+avagagner@users.noreply.github.com>
2022-02-22 21:13:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,106 @@
-var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
 
-var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"19dcff93eeb64f208d09d328656dd492" 
+        }
+      });
+      
+      var camera = new Camera({
+        position: [
+          -122.67904, // lon
+          45.539127, // lat
+          500// elevation in meters
+        ],
+        tilt:50,
+        heading: 210
+      });
 
-var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
 
-var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [bei, los, stl, mex].forEach(function(button) {
+      button.style.display = '.esri-button';
+      view.ui.add(button, 'bottom-right');
+    });
+    
+    bei.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: -122.6856,
+          y: 45.5427,
+          z: 300
+        },
+        tilt: 70,
+        heading: 170
+      });
+    });
+      
+     los.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: -122.685,
+          y: 45.542,
+          z: 100
+        },
+        tilt: 80,
+        heading: 170
+      });
+    });
+      
+           mex.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: -122.6845259,
+          y: 45.532964520,
+          z: 1500
+        },
+        tilt: 0,
+        heading: 0
+      });
+    });
+    
+      
+    stl.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
 
-var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
 
-var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
-
-
-
-marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
-marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
-marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
-marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
-marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
+    });
